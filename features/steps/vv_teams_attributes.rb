0fe20887a5c7d4df28cf83835ff0f750e6d71161
Anthony Scatchell <Scatchell@gmail.com>
2012-06-26 08:52:29 -0700

    Added some symbols in vv_teams_attributes instead of quoted strings

--- features/steps/vv_teams_attributes.rb
+++ features/steps/vv_teams_attributes.rb
@@ -1,8 +1,8 @@
 When /^I add Devs with the following attributes$/ do |table|
   table.hashes.each do |attrs|
-    @dev = Dev.new attrs["name"]
+    @dev = Dev.new attrs[:name]
     @dev.skill = attrs["skill level"].to_i
-    @dev.experience = attrs["experience"].to_i
+    @dev.experience = attrs[:experience].to_i
     @dev.typingSpeed = attrs["typing speed"].to_i
     
     @team.addDev(@dev)
